651cc28c8d9e0bc0fc22fe749f54c45959d01af2
Joseph Crail <jbcrail@gmail.com>
2018-05-01 14:59:35 -0700

    Add sample data

--- tests/test_intake_accumulo.py
+++ tests/test_intake_accumulo.py
@@ -7,10 +7,23 @@ from .utils import verify_plugin_interface, verify_datasource_interface
 
 @pytest.fixture(scope="module")
 def proxy():
+    from pyaccumulo import Accumulo, Mutation, Range
     from .utils import start_proxy, stop_proxy
 
     stop_proxy(let_fail=True)
     local_port = start_proxy()
+
+    c = Accumulo(host="localhost", port=int(local_port), user="root", password="secret")
+
+    table = "test"
+    if not c.table_exists(table):
+        c.create_table(table)
+
+    for num in range(0, 5):
+        m = Mutation("row_%d"%num)
+        m.put(cf="cf1", cq="cq1", val="%d"%num)
+        m.put(cf="cf2", cq="cq2", val="%d"%num)
+        c.write(table, m)
 
     try:
         yield local_port
@@ -39,6 +52,13 @@ def test_discover(proxy):
     info = src.discover()
     assert info['shape'] == (None, 6)
     assert info['npartitions'] == 1
+
+
+def test_read(proxy):
+    plugin = accumulo.Plugin()
+    src = plugin.open("test")
+    df = src.read()
+    assert len(df) == 10
 
 
 def test_close(proxy):
